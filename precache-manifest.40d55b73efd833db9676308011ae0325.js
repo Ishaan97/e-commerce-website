3650c3869983e3f4e385068d7b80fe2fb57191d4
Ishaan Gupta <igupta097@gmail.com>
2021-01-10 19:09:42 +0530

    Updates

---
+++ precache-manifest.40d55b73efd833db9676308011ae0325.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772bddbba26674b95f7e52e8c5dd0d1a",
+    "url": "/e-commerce-website-frontend/index.html"
+  },
+  {
+    "revision": "14ec9d6de0caf1038c59",
+    "url": "/e-commerce-website-frontend/static/css/main.d508d639.chunk.css"
+  },
+  {
+    "revision": "92738fc10e257f7f22ea",
+    "url": "/e-commerce-website-frontend/static/js/2.87674eb6.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/e-commerce-website-frontend/static/js/2.87674eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ec9d6de0caf1038c59",
+    "url": "/e-commerce-website-frontend/static/js/main.69fc99ae.chunk.js"
+  },
+  {
+    "revision": "d97f3064a39870644268",
+    "url": "/e-commerce-website-frontend/static/js/runtime-main.98e99ee4.js"
+  },
+  {
+    "revision": "f7dd71b5f3dfff430186a76f7e6868e2",
+    "url": "/e-commerce-website-frontend/static/media/iSkull-logo-black.f7dd71b5.png"
+  },
+  {
+    "revision": "ad8f33fc86beb7df4a97a124b6c6be71",
+    "url": "/e-commerce-website-frontend/static/media/iSkull-logo.ad8f33fc.png"
+  },
+  {
+    "revision": "67ba9c13c3d0ef9e0614eca45db0880c",
+    "url": "/e-commerce-website-frontend/static/media/shopping-cart.67ba9c13.svg"
+  },
+  {
+    "revision": "98153c406261edf1a9f80498ffebb9ea",
+    "url": "/e-commerce-website-frontend/static/media/user.98153c40.svg"
+  }
+]);
